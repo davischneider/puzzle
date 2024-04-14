40e72acc5ccc0515fad4392e492827e56afc4a8e
Davi Schneider <davi.schneider@sap.com>
2024-04-14 18:01:21 -0300

    Fix main loop

--- src/Application.java
+++ src/Application.java
@@ -9,8 +9,7 @@ public class Application {
     public static void main(String[] args) {
         int[][] firstState = {{7, 2, 4}, {5, 0, 6}, {8, 3, 1}};
         int[][] goalState = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
-
-        System.out.println(firstState[1][0]);
+//        int[][] goalState = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
 
         Puzzle puzzle = new Puzzle(firstState);
 
@@ -20,14 +19,24 @@ public class Application {
 
         while (!open.isEmpty()) {
             Puzzle currentState = open.poll();
+            closed.add(Arrays.deepToString(currentState.state()));
 
-            if (Arrays.equals(currentState.state(), goalState)) {
-                System.out.println("Solução Encontrada: " + Arrays.toString(currentState.state()));
+            if (Arrays.deepEquals(currentState.state(), goalState)) {
+                System.out.println("Found in: " + closed.size() + " possibilities");
+                System.out.println("Solution found: " + Arrays.deepToString(currentState.state()));
                 break;
             }
 
-            // Gerar filhos
-            List<Puzzle> nextStates = puzzle.nextStates();
+            List<Puzzle> nextStates = currentState.nextStates();
+            nextStates.forEach(puz -> {
+                if (!closed.contains(Arrays.deepToString(puz.state()))) {
+                    open.add(puz);
+                }
+            });
+
+            if (open.isEmpty()) {
+                System.out.println("There is no solution!");
+            }
 
         }
     }
